f52944ffd04101cbb7756c14cba9c26b693a8bb2
Maury <maurodamario86@gmail.com>
2024-10-30 09:51:37 -0300

    estilos

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	theme: {
+	  extend: {
+		fontFamily: {
+		  nexa: ['Nexa', 'sans-serif'],
+		},
+		colors: {
+		  background: 'var(--color-background)',
+		  accent: 'var(--color-accent)',
+		  neutral: 'var(--color-neutral)',
+		},
+	  },
+	},
+	// ... resto de tu configuración
+  }
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
