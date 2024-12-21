a3554189285d344fe008ae790cfff4c37b3a43a0
Lucas Henry <lucas.henrydz@gmail.com>
2024-12-21 01:33:25 -0300

    fix: minor issue with fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercelStatic from '@astrojs/vercel/static';
 
 export default defineConfig({
   integrations: [tailwind()],
+  output: 'static',
+  adapter: vercelStatic({
+    webAnalytics: {
+      enabled: true,
+    },
+    imageService: true,
+    maxDuration: 8,
+  }),
+  
 });
 
